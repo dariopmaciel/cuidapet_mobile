5c860c07951804f31ea2c6d9a4cdd547246e9d1f
dariopmaciel <dariodepaulamaciel@hotmail.com>
2024-06-24 15:45:14 -0300

    controllers do form de login + module + formulario de login

--- lib/app/modules/auth/login/login_module.dart
+++ lib/app/modules/auth/login/login_module.dart
@@ -1,14 +1,31 @@
-
+import 'package:cuidapet_mobile/app/modules/auth/login/login_controller.dart';
 import 'package:cuidapet_mobile/app/modules/auth/login/login_page.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-class LoginModule extends Module{
+class LoginModule extends Module {
+  @override
+  void binds(Injector i) {
+    //--------------ou este---------------------
+    // i.addLazySingleton(LoginController.new);
+    //--------------ou este---------------------
+    i.addLazySingleton(
+      () => LoginController(
+        userService: Modular.get(),
+        log: Modular.get(),
+      ),
+    );
+    //--------------ou este---------------------
+    // i.addLazySingleton(
+    //   () => LoginController(
+    //     userService: i(),
+    //     log: i(),
+    //   ),
+    // );
+  }
 
   @override
   void routes(RouteManager r) {
     //  r.child(Modular.initialRoute, child: (_) =>  LoginPage());
-    r.child(Modular.initialRoute, child: (_) =>  const LoginPage());
-
+    r.child(Modular.initialRoute, child: (_) => const LoginPage());
   }
-  
 }
--- lib/app/modules/auth/login/widgets/login_form.dart
+++ lib/app/modules/auth/login/widgets/login_form.dart
@@ -8,26 +8,58 @@ class _LoginForm extends StatefulWidget {
 }
 
 class _LoginFormState extends State<_LoginForm> {
+  final controller = Modular.get<LoginController>();
+  final _formKey = GlobalKey<FormState>();
+  final _loginEC = TextEditingController();
+  final _passwordEC = TextEditingController();
+
+  @override
+  void dispose() {
+    _loginEC.dispose();
+    _passwordEC.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Form(
-        child: Column(
-      children: [
-        CuidapetTextFormField(label: 'Login'),
-        const SizedBox(height: 20),
-        CuidapetTextFormField(label: 'Senha', obscureText: true),
-        const SizedBox(height: 20),
-        CuidapetDefaultButton(
-          label: 'Entrar',
-          onPressed: () {
-            // Messages.alert("MENSAGEM ERRADA!!!!");
-            Loader.show();
-            // () => Loader.hide(),
-            //ou assim
-            Future.delayed(const Duration(seconds: 2), Loader.hide);
-          },
-        ),
-      ],
-    ));
+      key: _formKey,
+      child: Column(
+        children: [
+          CuidapetTextFormField(
+            label: 'Login',
+            controller: _loginEC,
+            validator: Validatorless.multiple([
+              Validatorless.required("*Login Obrigatório!"),
+              Validatorless.email('*Email Inválido!'),
+            ]),
+          ),
+          const SizedBox(height: 20),
+          CuidapetTextFormField(
+            label: 'Senha',
+            obscureText: true,
+            controller: _passwordEC,
+            validator: Validatorless.multiple([
+              Validatorless.required("*Senha Obrigatória!"),
+              Validatorless.min(6, '*Mínimo de 6 caracteres!'),
+            ]),
+          ),
+          const SizedBox(height: 20),
+          CuidapetDefaultButton(
+            label: 'Entrar',
+            onPressed: () async {
+              final formValid = _formKey.currentState?.validate() ?? false;
+              if (formValid) {
+                await controller.login(_loginEC.text, _passwordEC.text);
+              }
+              // Loader.show();
+              // () => Loader.hide(),
+              //ou assim
+              Future.delayed(const Duration(seconds: 2), Loader.hide);
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
