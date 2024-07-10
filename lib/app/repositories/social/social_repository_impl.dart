62a1b579629aafd369b7de7f3472b98afc40bd28
dariopmaciel <dariodepaulamaciel@hotmail.com>
2024-07-10 14:51:47 -0300

    criando interfaces de login com facebook e google

---
+++ lib/app/repositories/social/social_repository_impl.dart
@@ -0,0 +1,17 @@
+import 'package:cuidapet_mobile/app/models/social_network_model.dart';
+
+import './social_repository.dart';
+
+class SocialRepositoryImpl extends SocialRepository {
+  @override
+  Future<SocialNetworkModel> facebookLogin() {
+    // TODO: implement facebookLogin
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<SocialNetworkModel> googleLogin() {
+    // TODO: implement googleLogin
+    throw UnimplementedError();
+  }
+}
