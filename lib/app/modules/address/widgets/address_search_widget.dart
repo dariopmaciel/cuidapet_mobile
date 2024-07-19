7073d920320f3aa365e20a2fd853e6f264f9b6f8
dariopmaciel <dariodepaulamaciel@hotmail.com>
2024-07-19 17:39:33 -0300

    Complente autocomplete privado

---
+++ lib/app/modules/address/widgets/address_search_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class _AddressSearchWidget extends StatefulWidget {
+
+  const _AddressSearchWidget({ super.key });
+
+  @override
+  State<_AddressSearchWidget> createState() => _AddressSearchWidgetState();
+}
+
+class _AddressSearchWidgetState extends State<_AddressSearchWidget> {
+
+   @override
+   Widget build(BuildContext context) {
+       return Scaffold(
+           appBar: AppBar(title: const Text(''),),
+           body: Container(),
+       );
+  }
+}
