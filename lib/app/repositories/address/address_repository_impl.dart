bae314fec57a26b6636a93d9d53c275eb2b2b6f0
dariopmaciel <dariodepaulamaciel@hotmail.com>
2024-07-23 15:09:37 -0300

    Integrando com googlePlaces

--- lib/app/repositories/address/address_repository_impl.dart
+++ lib/app/repositories/address/address_repository_impl.dart
@@ -1,18 +1,37 @@
+import 'package:cuidapet_mobile/app/core/exceptions/failure.dart';
+import 'package:cuidapet_mobile/app/core/helpers/environments.dart';
 import 'package:cuidapet_mobile/app/models/place_model.dart';
+import 'package:google_places/google_places.dart';
 
 import './address_repository.dart';
 
 class AddressRepositoryImpl extends AddressRepository {
   @override
-  Future<List<PlaceModel>> findAddressGooglePlaces(String addressPattern) {
-    
+  Future<List<PlaceModel>> findAddressGooglePlaces(
+      String addressPattern) async {
+    final googleApiKey = Environments.param('google_api_key');
 
-    
+    if (googleApiKey == null) {
+      throw Failure(message: "Google API Key NOT FOUND!!!");
+    }
 
+    final googlePlaces = GooglePlaces(googleApiKey);
+    final addressResult =
+        await googlePlaces.search.getTextSearch(addressPattern);
 
+    final candidates = addressResult?.results;
 
-
-    throw UnimplementedError();
+    if (candidates != null) {
+      return candidates.map<PlaceModel>((searchResult) {
+        final location = searchResult.geometry?.location;
+        final address = searchResult.formattedAddress;
+        return PlaceModel(
+          address: address ?? '',
+          lat: location?.lat ?? 0,
+          lng: location?.lng ?? 0,
+        );
+      }).toList();
+    }
+    return <PlaceModel>[];
   }
-
 }
