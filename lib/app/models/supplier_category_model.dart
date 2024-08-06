b657a7c6bb62d3b10fec98d8963ccccd38f11339
dariopmaciel <dariodepaulamaciel@hotmail.com>
2024-08-06 19:18:10 -0300

    model

--- lib/app/models/supplier_category_model.dart
+++ lib/app/models/supplier_category_model.dart
@@ -1,13 +1,35 @@
+import 'dart:convert';
+
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 
 class SupplierCategoryModel {
   int id;
-  String nome;
+  String name;
   String type;
 
   SupplierCategoryModel({
     required this.id,
-    required this.nome,
+    required this.name,
     required this.type,
   });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'type': type,
+    };
+  }
+
+  factory SupplierCategoryModel.fromMap(Map<String, dynamic> map) {
+    return SupplierCategoryModel(
+      id: (map['id'] ?? 0) as int,
+      name: (map['name'] ?? '') as String,
+      type: (map['type'] ?? '') as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory SupplierCategoryModel.fromJson(String source) => SupplierCategoryModel.fromMap(json.decode(source) as Map<String, dynamic>);
 }
