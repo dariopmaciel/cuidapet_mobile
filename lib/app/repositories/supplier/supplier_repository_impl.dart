e8ec4a333ff5b71577b3e4384d3929f10171ba1e
dariopmaciel <dariodepaulamaciel@hotmail.com>
2024-08-06 19:32:02 -0300

    busca de categoria

--- lib/app/repositories/supplier/supplier_repository_impl.dart
+++ lib/app/repositories/supplier/supplier_repository_impl.dart
@@ -1,6 +1,35 @@
+import 'package:cuidapet_mobile/app/core/exceptions/failure.dart';
+import 'package:cuidapet_mobile/app/core/logger/app_logger.dart';
+import 'package:cuidapet_mobile/app/core/rest_client/rest_client.dart';
+import 'package:cuidapet_mobile/app/core/rest_client/rest_cliente_exception.dart';
+import 'package:cuidapet_mobile/app/models/supplier_category_model.dart';
+
 import './supplier_repository.dart';
 
 class SupplierRepositoryImpl extends SupplierRepository {
-  
+  final RestClient _restClient;
+  final AppLogger _logger;
 
+  SupplierRepositoryImpl({
+    required RestClient restClient,
+    required AppLogger logger,
+  })  : _restClient = restClient,
+        _logger = logger;
+
+  @override
+  Future<List<SupplierCategoryModel>> getCategories() async {
+    try {
+      final result = await _restClient.auth().get('/categories/');
+      return result.data
+          .map<SupplierCategoryModel>(
+            (categoryResponse) =>
+                SupplierCategoryModel.fromMap(categoryResponse),
+          )
+          .toList();
+    } on RestClientException catch (e, s) {
+      const message = 'Erro ao buscar categorias dos fornecedores';
+      _logger.error(message, e, s);
+      throw Failure(message: message);
+    }
+  }
 }
