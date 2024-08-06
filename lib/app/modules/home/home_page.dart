95edfce3b8b017fe6600c2e34238a243ae0c7dda
dariopmaciel <dariodepaulamaciel@hotmail.com>
2024-08-06 14:12:47 -0300

    appbar ok

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,14 +1,8 @@
 import 'package:cuidapet_mobile/app/core/life_cycle/page_life_cycle_state.dart';
-
 import 'package:cuidapet_mobile/app/entities/address_entity.dart';
-// import 'package:cuidapet_mobile/app/core/rest_client/rest_client.dart';
 import 'package:cuidapet_mobile/app/modules/home/home_controller.dart';
-import 'package:cuidapet_mobile/app/services/address/address_service.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cuidapet_mobile/app/modules/home/widgets/home_app_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:flutter_modular/flutter_modular.dart';
-// import 'package:flutter_modular/flutter_modular.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -28,58 +22,65 @@ class _HomePageState extends PageLifeCycleState<HomeController, HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomePage'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              child: const Text('Logout'),
-            ),
-            TextButton(
-              onPressed: () async {
-                // final categoriesResponse = await Modular.get<RestClient>().auth().get('/categories/');
-                // debugPrint(categoriesResponse.data);
-                Navigator.pushNamed(context, '/address/');
-              },
-              child: const Text('Teste de Refresh Token'),
-            ),
-            //!----------
-            TextButton(
-              onPressed: () async {
-                // await Modular.to.pushNamed('/address/');
-                controller.goToAddressPage();
-              },
-              child: const Text('Ir para endereço'),
-            ),
-            //!----------
-            TextButton(
-              onPressed: () async {
-                final address = await Modular.get<AddressService>().getAddressSelected();
-                setState(() {
-                  addressEntity = address;
-                });
-              },
-              child: const Text('Buscar endereço'),
-            ),
-            Observer(
-                builder: (_) {
-                    return Text(controller.addressEntity?.address ?? "Nenhum Endereço Selecionado");
-                },
-            ),
-            Observer(
-                builder: (_) {
-                    return Text(controller.addressEntity?.additional ?? "Nenhum Complemento Selecionado");
-                },
-            )
-          ],
-        ),
+      drawer: const Drawer(),
+      backgroundColor: Colors.grey[100],
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) {
+          //item sem scroll vertical, rolagem pequena
+          return <Widget>[
+             const HomeAppBar(),
+          ];
+        },
+        body: Container(),
       ),
     );
   }
 }
+/*
+// mainAxisAlignment: MainAxisAlignment.center,
+          // children: [
+          //   TextButton(
+          //     onPressed: () {
+          //       FirebaseAuth.instance.signOut();
+          //     },
+          //     child: const Text('Logout'),
+          //   ),
+          //   TextButton(
+          //     onPressed: () async {
+          //       // final categoriesResponse = await Modular.get<RestClient>().auth().get('/categories/');
+          //       // debugPrint(categoriesResponse.data);
+          //       Navigator.pushNamed(context, '/address/');
+          //     },
+          //     child: const Text('Teste de Refresh Token'),
+          //   ),
+          //   //!----------
+          //   TextButton(
+          //     onPressed: () async {
+          //       // await Modular.to.pushNamed('/address/');
+          //       controller.goToAddressPage();
+          //     },
+          //     child: const Text('Ir para endereço'),
+          //   ),
+          //   //!----------
+          //   TextButton(
+          //     onPressed: () async {
+          //       final address = await Modular.get<AddressService>().getAddressSelected();
+          //       setState(() {
+          //         addressEntity = address;
+          //       });
+          //     },
+          //     child: const Text('Buscar endereço'),
+          //   ),
+          //   Observer(
+          //       builder: (_) {
+          //           return Text(controller.addressEntity?.address ?? "Nenhum Endereço Selecionado");
+          //       },
+          //   ),
+          //   Observer(
+          //       builder: (_) {
+          //           return Text(controller.addressEntity?.additional ?? "Nenhum Complemento Selecionado");
+          //       },
+          //   )
+          // ],
+
+ */
