28a77e288e0a03e9e8e1fca3d9315e967f7875ea
dariopmaciel <dariodepaulamaciel@hotmail.com>
2024-08-08 16:01:23 -0300

    implementaão de lista de petshops funcionando, sem carregamento

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:cuidapet_mobile/app/core/life_cycle/page_life_cycle_state.dart';
+import 'package:cuidapet_mobile/app/core/ui/extensions/size_screen_extension.dart';
 import 'package:cuidapet_mobile/app/core/ui/extensions/theme_extension.dart';
 import 'package:cuidapet_mobile/app/entities/address_entity.dart';
 import 'package:cuidapet_mobile/app/models/supplier_category_model.dart';
--- lib/app/modules/home/widgets/home_supplier_tab.dart
+++ lib/app/modules/home/widgets/home_supplier_tab.dart
@@ -14,16 +14,18 @@ class _HomeSupplierTab extends StatelessWidget {
     return Column(
       children: [
         _HomeTabHeader(homeController: homeController),
-        Observer(
-          builder: (_) {
-            return AnimatedSwitcher(
-              duration: const Duration(milliseconds: 1000),
-              child: homeController.supplierPageTypeSelected ==
-                      SupplierPageType.list
-                  ? const _HomeSuppplierList()
-                  : const _HomeSupplierGrid(),
-            );
-          },
+        Expanded(
+          child: Observer(
+            builder: (_) {
+              return AnimatedSwitcher(
+                duration: const Duration(milliseconds: 400),
+                child: homeController.supplierPageTypeSelected ==
+                        SupplierPageType.list
+                    ? const _HomeSuppplierList()
+                    : const _HomeSupplierGrid(),
+              );
+            },
+          ),
         )
       ],
     );
@@ -83,7 +85,17 @@ class _HomeSuppplierList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('SupplierList');
+    return CustomScrollView(
+      slivers: [
+        SliverList(
+            delegate: SliverChildBuilderDelegate(
+          childCount: 10,
+          (context, index) {
+            return const _HomeSuppplierListItemWidget();
+          },
+        ))
+      ],
+    );
   }
 }
 
@@ -93,5 +105,102 @@ class _HomeSupplierGrid extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Text("SupplierGrid");
+  }
+}
+
+class _HomeSuppplierListItemWidget extends StatelessWidget {
+  const _HomeSuppplierListItemWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      child: Stack(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(left: 30),
+            width: 1.sw,
+            height: 80.h,
+            decoration: BoxDecoration(
+              color: Colors.grey[300],
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    // alignment: Alignment.centerLeft,
+                    margin: const EdgeInsets.only(left: 50),
+                    // color: Colors.blue,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(left: 5),
+                          child: Text(
+                            "Clinica Central ABC",
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          children: [
+                            Icon(Icons.location_on, size: 25.r),
+                            const Text('1.34Km de distancia')
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  child: CircleAvatar(
+                    backgroundColor: context.primaryColor,
+                    maxRadius: 15,
+                    child: const Icon(
+                      Icons.arrow_forward_ios,
+                      color: Colors.white,
+                      size: 15,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 5),
+            width: 70,
+            height: 70,
+            decoration: BoxDecoration(
+                color: Colors.transparent,
+                // color: Colors.red,
+                border: Border.all(
+                  width: 1,
+                  color: Colors.transparent,
+                  // color: Colors.black,
+                ),
+                borderRadius: BorderRadius.circular(100)),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey[100]!,
+                  width: 5,
+                ),
+                color: Colors.white,
+                // color: Colors.pink,
+                borderRadius: BorderRadius.circular(100),
+                image: const DecorationImage(
+                  // image: AssetImage('assets/images/logo.png'),
+                  image: NetworkImage('https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcS_ovn1hkTKcT5UdQE6QCuSNKJAfQKhpx23bYpcz9v-axoOCNg7'),
+                  fit: BoxFit.contain
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
